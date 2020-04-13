d981510416a54cfe5ad37d6957b14a2bea72382f
haohan <haohan0307@163.com>
2020-04-13 12:52:27 +0800

    强软弱虚四种引用例子

---
+++ src/main/java/com/haohan/FourReference/SoftReferenceTest.java
@@ -0,0 +1,31 @@
+package com.haohan.FourReference;
+
+import java.lang.ref.SoftReference;
+
+/**
+ * @author haohan
+ * 04/13/2020 - 09:45 上午
+ * 软引用 --- 只有在内存不足时才会被GC回收
+ */
+public class SoftReferenceTest {
+
+    public static void main(String[] args) {
+        //模拟缓存
+        byte[] bytes = new byte[100 * 1024 * 1024];
+        SoftReference<byte[]> cacheRef = new SoftReference<byte[]>(bytes);
+
+        System.out.println(cacheRef.get());
+        //第一次GC
+        System.gc();
+        cacheRef = null;
+
+        System.out.println(cacheRef.get());
+
+        //第二次GC
+        System.gc();
+
+        System.out.println(cacheRef.get());
+
+    }
+
+}
